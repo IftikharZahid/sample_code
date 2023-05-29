e0091cccef687f826846bc8cca0e429adbf1d5d6
Iftikhar Zahid <73811545+IftikharZahid@users.noreply.github.com>
2023-05-29 09:05:14 +0500

    Create HomeScreen.jsx

---
+++ HomeScreen.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+function HomeScreen(navigation) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>HomeScreen!</Text>
+    </View>
+  );
+}
+export { HomeScreen };
